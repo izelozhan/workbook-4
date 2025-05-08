3ba7fbaa8f4694101616f24d002b7ad60c039935
Izel Ozhan <izelozhan@gmail.com>
2025-05-08 10:47:14 -0500

    workbook-4 static-exercise-1

---
+++ StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class NameFormatter {
+
+    private NameFormatter() {
+    }
+
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
+        String fullName = "";
+
+        if (prefix != null && !prefix.isEmpty()) {
+            fullName += prefix + ". ";
+        }
+        if (firstName != null && !firstName.isEmpty()) {
+            fullName += firstName + " ";
+        }
+        if (middleName != null && !middleName.isEmpty()) {
+            fullName += middleName + " ";
+        }
+        if (lastName != null && !lastName.isEmpty()) {
+            fullName += lastName + " ";
+        }
+        if (suffix != null && !suffix.isEmpty()) {
+            fullName += suffix + ", ";
+        }
+
+        return fullName;
+    }
+}
